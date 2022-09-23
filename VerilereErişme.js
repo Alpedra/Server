af8794312526de8c74ea7bca3ffa9cb9735657f1
Alpedra <musacanbacak22@gmail.com>
2022-09-23 15:26:23 +0300

    Tamamlandı

--- VerilereErişme.js
+++ VerilereErişme.js
@@ -5,3 +5,13 @@ const database=require('./Database.json')
 app.get('/',(req,res)=>{
     res.send('Hoş geldiniz')
 })
+
+app.get('/users/:uid',(req,res)=>{
+    let founduser=database.Users.find(x,(x.Id==req.params.uid))
+    let jsonstring=JSON.stringify(founduser)
+    res.send(jsonstring)
+})
+
+app.listen(3000,(req,res)=>{
+   console.log('Server başlatıldı')
+})
